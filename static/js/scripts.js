52436fc4ae31ca631f6ec0aab83b33f650f247c2
EVEN17xxx <143746864+EVEN17xxx@users.noreply.github.com>
2025-05-21 23:28:13 +0200

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'projects', 'awards']
+const section_names = ['home', 'projects', 'awards', 'experience']
 
 
 window.addEventListener('DOMContentLoaded', event => {
